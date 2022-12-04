4b22ca7d4db7c10db12ee35abd7b7d20fb99294a
Arif5577 <arifalamm33@gmail.com>
2022-12-04 13:35:43 +0600

    firebase added for hosting

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOeBrNqkEZL4hYaesBgrx3SErSJkCW3io",
+  authDomain: "newsman-d0c55.firebaseapp.com",
+  projectId: "newsman-d0c55",
+  storageBucket: "newsman-d0c55.appspot.com",
+  messagingSenderId: "712393846651",
+  appId: "1:712393846651:web:0f65712f1a692e299557de",
+};
+
+const app = initializeApp(firebaseConfig);
